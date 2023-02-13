991af3b14646cb48f161b1becb2734d395938a2c
itsmorbintime123 <124811893+itsmorbintime123@users.noreply.github.com>
2023-02-13 15:37:53 +0100

    Monster constructor

--- Game/Monster.cs
+++ Game/Monster.cs
@@ -2,5 +2,18 @@ namespace Game;
 
 public class Monster
 {
-    
+    public int ID;
+    public string Name;
+    public string NamePlural;
+    public int MaximumDamage;
+    public int RewardExperience;
+    public int RewardGold;
+    public Loot CountedItemList;
+    public int CurrentHitPoints;
+
+    public Monster()
+  {
+    //put constructor code here
+  }
+
 }
