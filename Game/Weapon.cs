3e2481ca805be0b665348ab337bedcf183040773
Joost-Reintjes <95181999+Joost-Reintjes@users.noreply.github.com>
2023-02-13 15:16:10 +0100

    Added constructor to weapons class

--- Game/Weapon.cs
+++ Game/Weapon.cs
@@ -2,5 +2,20 @@ namespace Game;
 
 public class Weapon
 {
+    public int ID;
+    public string name;
+    public string plural;
+    public int value1;
+    public int value2;
+
+    public Weapon(int weapon_id, string name, string plural, int value1, int value2)
+    {
+        this.ID = weapon_id;
+        this.name = name;
+        this.plural = plural;
+        this.value1 = value1;
+        this.value2 = value2;
+        
+    }
     
 }
