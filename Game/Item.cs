f88d37675756b260b71f09219e99a87ccd3df76d
Dimitrey <1054272@hr.nl>
2023-02-13 15:07:27 +0100

    edit field and constructor for item

--- Game/Item.cs
+++ Game/Item.cs
@@ -1,6 +1,15 @@
 namespace Game;
 
-public class Item
+class Item
 {
-    
+    public int ID;
+    public string Name;
+    public string PluralName;
+
+    public Item(int id, string name, string pluralName)
+    {
+        ID = id;
+        Name = name;
+        PluralName = pluralName;
+    }
 }
