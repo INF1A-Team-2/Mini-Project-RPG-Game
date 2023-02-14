aeab0676096c1492f5ef86bdc2f157e1799c7205
ZoutigeWolf <guuskamphuis@gmail.com>
2023-02-14 16:09:40 +0100

    Add AddItem method to CountedItemList

--- Game/CountedItemList.cs
+++ Game/CountedItemList.cs
@@ -3,4 +3,9 @@ namespace Game;
 public class CountedItemList
 {
     public List<CountedItem> ItemList = new List<CountedItem>();
+
+    public void AddItem(CountedItem item)
+    {
+        ItemList.Add(item);
+    }
 }
