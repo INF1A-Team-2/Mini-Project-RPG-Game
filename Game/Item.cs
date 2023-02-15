74984b801db4762e5409c7502e36b2b4854ed218
Dimitrey <1054272@hr.nl>
2023-02-15 15:12:59 +0100

    small update

--- Game/Item.cs
+++ Game/Item.cs
@@ -1,15 +1,17 @@
+using System.Runtime.CompilerServices;
+
 namespace Game;
 
 class Item
 {
-    this.ID = id;
-    this.Name = name;
-    this.PluralName = pluralName;
+    public int ID;
+    public string Name;
+    public string PluralName;
 
     public Item(int id, string namee, string pluralName)
     {
-        ID = id;
-        Name = namee;
-        PluralName = pluralName;
+        this.ID = id;
+        this.Name = namee;
+        this.PluralName = pluralName;
     }
 }
