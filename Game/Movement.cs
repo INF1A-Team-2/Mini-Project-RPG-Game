a1aa578d4a1080159ef3946b4227642cf5a50b8c
PieterLandaal <0981786@hr.nl>
2023-02-16 13:22:33 +0100

    Program.player in Movement.cs

--- Game/Movement.cs
+++ Game/Movement.cs
@@ -6,16 +6,16 @@ class Movement
     {
         Console.WriteLine("Where do you want to go? (N/E/S/W)");
         string direction = Console.ReadLine().ToUpper();
-        TryMoveTo(player.CurrentLocation.GetLocationAt(direction), player);
+        TryMoveTo(player.CurrentLocation.GetLocationAt(direction));
         Console.WriteLine("Current location: " + player.CurrentLocation.Name);
         Console.WriteLine(player.CurrentLocation.Compass());
     }
     
-    public bool TryMoveTo(Location newLocation, Player player)
+    public bool TryMoveTo(Location newLocation)
     {
         if (newLocation != null)
         {
-            player.CurrentLocation = newLocation;
+            Program.player.CurrentLocation = newLocation;
             return true;
         }
         return false;
